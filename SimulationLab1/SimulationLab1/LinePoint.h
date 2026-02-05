d68b7103977468979e5ca75a31da8b71eff3914e
AnimeAdam <A.SZLAMP-2023@hull.ac.uk>
2026-02-05 01:28:18 +0000

    A small update

--- SimulationLab1/SimulationLab1/LinePoint.h
+++ SimulationLab1/SimulationLab1/LinePoint.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
+class LinePoint
+{
+public:
+	LinePoint(glm::vec3 dir_, glm::vec3 startPt_, glm::vec3 genPt_) : direction(dir_), startingPoint(startPt_), generalPoint(genPt_) {}
+	~LinePoint();
+
+
+
+private:
+	glm::vec3 direction;
+	glm::vec3 startingPoint;
+	glm::vec3 generalPoint;
+};
