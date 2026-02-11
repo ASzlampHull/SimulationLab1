a2578aa9a4008f10c3101bde4c46bb16daabf5e4
AnimeAdam <A.SZLAMP-2023@hull.ac.uk>
2026-02-11 16:42:01 +0000

    Finished Q5

--- SimulationLab1/GoogleTest/test.cpp
+++ SimulationLab1/GoogleTest/test.cpp
@@ -3,8 +3,10 @@
 #include "../SimulationLab1/LinePoint.h"
 #include "../SimulationLab1/SphereLine.h"
 #include "../SimulationLab1/PointToPlane.h"
+#include "../SimulationLab1/SpherePlane.h"
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
+#include <cmath>
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
@@ -113,9 +115,73 @@ TEST(SphereLineIntersection, PassThroughCentreSphere) {
 
 #pragma region Q4 Closest Distance from a Point to a Plane
 
-TEST(ClosestDistanceToPlane, ClosePointOnPlane) {
+TEST(ClosestDistanceToPlane, PointAbovePlane) {
 	PointToPlane pointToPlane(glm::vec3(0, 0, 0), glm::vec3(0, 0, 1), glm::vec3(2, 3, 5));
 	EXPECT_FLOAT_EQ(pointToPlane.ClosetDistanceToPlane(), 5.0f);
+}
+
+TEST(ClosestDistanceToPlane, PointBelowPlane) {
+	PointToPlane pointToPlane(glm::vec3(0, 0, 0), glm::vec3(0, 0, 1), glm::vec3(2, 3, -4));
+	EXPECT_FLOAT_EQ(pointToPlane.ClosetDistanceToPlane(), 4.0f);
+}
+
+TEST(ClosestDistanceToPlane, PointOnPlane) {
+	glm::vec3 planeNormal = glm::vec3(1, 1, 1);
+	planeNormal *= (1.0f / std::sqrt(3.0f));
+	PointToPlane pointToPlane(glm::vec3(1, 1, 1), planeNormal, glm::vec3(0, 2, 1));
+	EXPECT_FLOAT_EQ(pointToPlane.ClosetDistanceToPlane(), 0.0f);
+}
+
+TEST(ClosestDistanceToPlane, PointCloseToPlane) {
+	glm::vec3 planeNormal = glm::vec3(1, 1, 0);
+	planeNormal *= (1.0f / std::sqrt(2.0f));
+	PointToPlane pointToPlane(glm::vec3(0, 0, 0), planeNormal, glm::vec3(1, 1, 1));
+	EXPECT_FLOAT_EQ(pointToPlane.ClosetDistanceToPlane(), 1.41f);
+}
+
+TEST(ClosestDistanceToPlane, PointNegativeCoordinates) {
+	glm::vec3 planeNormal = glm::vec3(1, 1, 1);
+	planeNormal *= (1.0f / std::sqrt(3.0f));
+	PointToPlane pointToPlane(glm::vec3(-2, -2, -2), planeNormal, glm::vec3(-1, -1, -1));
+	EXPECT_FLOAT_EQ(pointToPlane.ClosetDistanceToPlane(), 1.73f);
+}
+
+TEST(ClosestDistanceToPlane, PointAlongNormals) {
+	glm::vec3 planeNormal = glm::vec3(1, 1, 0);
+	planeNormal *= (1.0f / std::sqrt(2.0f));
+	PointToPlane pointToPlane(glm::vec3(0, 0, 0), planeNormal, glm::vec3(1, 1, 0));
+	EXPECT_FLOAT_EQ(pointToPlane.ClosetDistanceToPlane(), 1.41f);
+}
+
+TEST(ClosestDistanceToPlane, PointRandomPlane) {
+	glm::vec3 planeNormal = glm::vec3(1, -1, 0);
+	planeNormal *= (1.0f / std::sqrt(2.0f));
+	PointToPlane pointToPlane(glm::vec3(0, 0, 0), planeNormal, glm::vec3(1, 2, 3));
+	EXPECT_FLOAT_EQ(pointToPlane.ClosetDistanceToPlane(), 0.71f);
+}
+
+#pragma endregion
+
+#pragma region Q5 Sphere to Plane Collision
+
+TEST(SpherePlaneCollision, NoIntersection) {
+	SpherePlane spherePlane(glm::vec3(0, 0, 5), glm::vec3(0, 0, 1), glm::vec3(0, 0, 0), 2);
+	EXPECT_FALSE(spherePlane.DoesClosetPointIntersectSphere());
+}
+
+TEST(SpherePlaneCollision, TouchingPlane) {
+	SpherePlane spherePlane(glm::vec3(0, 0, 2), glm::vec3(0, 0, 1), glm::vec3(0, 0, 0), 2);
+	EXPECT_TRUE(spherePlane.DoesClosetPointIntersectSphere());
+}
+
+TEST(SpherePlaneCollision, IntersectingPlane) {
+	SpherePlane spherePlane(glm::vec3(0, 0, 1), glm::vec3(0, 0, 1), glm::vec3(0, 0, 0), 2);
+	EXPECT_TRUE(spherePlane.DoesClosetPointIntersectSphere());
+}
+
+TEST(SpherePlaneCollision, ContainedInPlane) {
+	SpherePlane spherePlane(glm::vec3(0, 0, 0), glm::vec3(0, 0, 1), glm::vec3(0, 0, 0), 2);
+	EXPECT_TRUE(spherePlane.DoesClosetPointIntersectSphere());
 }
 
 #pragma endregion
--- SimulationLab1/SimulationLab1/PointToPlane.cpp
+++ SimulationLab1/SimulationLab1/PointToPlane.cpp
@@ -4,7 +4,7 @@
 float PointToPlane::ClosetDistanceToPlane()
 {
 	glm::vec3 pointToPlane = generalPoint - startingPoint;
-	float distance = glm::dot(pointToPlane, normalizedDirection);
+	float distance = glm::dot(pointToPlane, normalizedPlane);
 	float roundedDistance = std::round(std::abs(distance) * 100.0f) / 100.0f;
 	return roundedDistance;
 }
